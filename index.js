fa64d5b914d4e8573c720d281e0f8fdee237faeb
Vitor <130366864+vitormanoeloliveiramattos@users.noreply.github.com>
2024-10-04 14:59:04 +0000

    criada as rotas home e sobre iniciado o menu

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = 3305;
 
 app.get("/", function (req, res) {
-  //res.sendFile();
   res.sendFile(__dirname + "/src/views/index.html");
 });
 
@@ -12,9 +11,9 @@ app.get("/sobre", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
 });
 
-app.get("/erro", function (req, res) {
-  res.sendFile(__dirname + "/src/views/404.html");
-  // res.sendFile(__dirname + "/src/views/404.html");
+// rota utilizando middleware (404)
+app.use(function (req, res) {
+  res.status(404).sendfile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
